d033e8bba3943e6dbbc850b357348e745222284c
Luffy <othmaneboughari@gmail.com>
2024-12-31 00:27:06 +0100

    Create img-gimage.js

---
+++ plugins/img-gimage.js
@@ -0,0 +1,13 @@
+import fg from 'api-dylux'
+let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
+  if (!text) throw `✳️ Ingrese la imagen que quiere buscar \n\n📌 Ejemplo: *${usedPrefix + command}* Billie Eilish`
+  let res = await fg.googleImage(text)
+  conn.sendFile(m.chat, res.getRandom(), 'img.png', `
+✅ Resultado de : *${text}*`.trim(), m)
+}
+handler.help = ['imagen']
+handler.tags = ['img']
+handler.command = /^(img|image|gimage|imagen)$/i
+handler.diamond = true
+
+export default handler
