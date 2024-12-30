63c2d8983face7cc97d9e4d480b8adc3a9c384f7
Luffy <othmaneboughari@gmail.com>
2024-12-30 23:24:02 +0100

    Create dl-twitter.js

---
+++ plugins/dl-twitter.js
@@ -0,0 +1,23 @@
+import fg from 'api-dylux'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `📌 Ejemplo : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
+          m.react(rwait)    
+          try {
+          let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
+          let te = ` 
+┌─⊷ *TWITTER DL*
+▢ Descripción: ${desc}
+└───────────`
+conn.sendFile(m.chat, HD, 'twitter.mp4', te, m)
+m.react(done)
+} catch (e) {
+  	m.reply(`✳️ verifica que el link sea de Twitter`)
+	} 
+	
+}
+handler.help = ['twitter'].map(v => v + ' <url>')
+handler.tags = ['dl']
+handler.command = /^(twitter|tw)$/i
+handler.diamond = true
+
+export default handler
