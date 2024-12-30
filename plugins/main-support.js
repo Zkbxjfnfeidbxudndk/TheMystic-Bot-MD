434a2e4f930a4bbc2bc61c9eabdd58702a405f12
Luffy <othmaneboughari@gmail.com>
2024-12-31 00:41:26 +0100

    Create main-support.js

---
+++ plugins/main-support.js
@@ -0,0 +1,38 @@
+let handler = async (m, { conn }) => {
+
+m.reply(`
+≡  *luffyᴮᴼᵀ ┃ SUPPORT*
+
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ Grupo *1*
+${dygp}
+
+▢ Grupo Bot  
+https://chat.whatsapp.com/KlGxXzUATiFCekXgHoSdHm
+
+▢ Grupo bot 2
+
+
+▢ 𝐌𝐘 - 𝐌𝐚𝐲𝐥𝐮𝐱 | ᴮᴼᵀ⚡
+
+
+▢ 📲💻Melim bot channel 🎬🎮
+
+https://whatsapp.com/channel/0029Vb1w26yGOj9eWm2FLa1N
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ Todos los Grupos
+ https://instabio.cc/fg98ff
+
+▢ *Telegram*
+• https://t.me/fgawgp
+ ▢ *PayPal*
+• https://paypal.me/fg98f
+▢ *YouTube*
+• https://www.youtube.com/fg98f`)
+
+}
+handler.help = ['support']
+handler.tags = ['main']
+handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux', 'support'] 
+
+export default handler
