d224dec617edc03bbc30b69029dc8f36575ab211
Luffy <othmaneboughari@gmail.com>
2024-12-30 23:28:52 +0100

    Create econ-balance.js

---
+++ plugins/econ-balance.js
@@ -0,0 +1,22 @@
+let handler = async (m, {conn, usedPrefix}) => {
+	
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let user = global.db.data.users[who]
+    if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+    conn.reply(m.chat, `
+┌───⊷ *BALANCE* ⊶
+▢ *📌Nombre* : _@${who.split('@')[0]}_
+▢ *💎Diamantes* : _${user.diamond}_
+▢ *⬆️XP* : _Total ${user.exp}_
+└──────────────
+
+*NOTA :* 
+Puedes comprar 💎 diamantes usando los comandos
+❏ *${usedPrefix}buy <cantidad>*
+❏ *${usedPrefix}buyall*`, m, { mentions: [who] })
+}
+handler.help = ['balance']
+handler.tags = ['econ']
+handler.command = ['bal', 'diamantes', 'diamond', 'balance'] 
+
+export default handler
