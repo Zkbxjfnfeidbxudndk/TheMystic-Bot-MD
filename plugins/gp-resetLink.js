59f251d16680f660f06c2dfc3b83182b8e176d7f
Luffy <othmaneboughari@gmail.com>
2024-12-31 00:19:59 +0100

    Create gp-resetLink.js

---
+++ plugins/gp-resetLink.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+  let res = await conn.groupRevokeInvite(m.chat)
+  m.reply('✅ El enlace de grupo se ha restablecido correctamente\n\n📌 Enlace nuevo:\nhttps://chat.whatsapp.com/' + res)
+}
+handler.help = ['resetlink']
+handler.tags = ['group']
+handler.command = ['revoke', 'resetlink', 'anularlink'] 
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
